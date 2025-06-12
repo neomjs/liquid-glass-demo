36bb8d52981eda3ba1f028ee15bb8c2d2c1a3b07
tobiu <tobiasuhlig78@gmail.com>
2025-06-13 01:42:55 +0200

    MyApp.view.GlassComponent

--- apps/myapp/view/Viewport.mjs
+++ apps/myapp/view/Viewport.mjs
@@ -1,6 +1,5 @@
-import BaseViewport from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
-import Component    from '../../../node_modules/neo.mjs/src/component/Base.mjs';
-import TabContainer from '../../../node_modules/neo.mjs/src/tab/Container.mjs';
+import BaseViewport   from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
+import GlassComponent from './GlassComponent.mjs';
 
 /**
  * @class MyApp.view.Viewport
@@ -17,35 +16,11 @@ class Viewport extends BaseViewport {
          * @member {Object} layout={ntype:'fit'}
          */
         layout: {ntype: 'fit'},
-
         /**
          * @member {Object[]} items
          */
         items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
-
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
-            },
-
-            items: [{
-                header: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
-            }, {
-                header: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
-            }]
+            module: GlassComponent
         }]
     }
 }
